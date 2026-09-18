8b1c7ef80b6bb30c83158b191f9e7e8d1841429b
juanandresospina1633 <juanandresospina1633@gmail.com>
2026-09-18 16:33:37 -0500

    carga de datos y codigo de la plantilla

--- Grupo06_Laboratorio07.py
+++ Grupo06_Laboratorio07.py
@@ -1,1 +1,95 @@
+# Carga de librerías 
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt
 
+# Carga de datos 
+df = pd.read_csv("smart_workout_raw_dataset.csv") 
+# Dimensiones del DataFrame 
+df.shape 
+# Impresión de las primeras filas 
+df.head() 
+# Identificación del tipo de cada variable 
+df.info() 
+# Creando nuevas variables con valores en minúsculas y sin espacios vacíos 
+categorical_columns = [ 
+"persona", 
+"fitness_goal", 
+"experience_level", 
+"injury_constraint", 
+"bodyPart", 
+"equipment", 
+"target_muscle" 
+] 
+for column in categorical_columns: 
+    df[column + "_std"] = ( 
+    df[column] 
+    .astype("string") 
+    .str.strip() 
+    .str.lower() 
+    .str.replace(" ", "_") 
+    ) 
+df[["fitness_goal", "fitness_goal_std"]].drop_duplicates().sort_values( 
+"fitness_goal_std" 
+) 
+df_usuario = ( 
+df.groupby("user_id") 
+.agg( 
+age = ("age", "median"), 
+n_records = ("user_id", "size"), 
+n_exercises = ("exercise_id", "nunique"), 
+n_bodyparts = ("bodyPart_std", "nunique"), 
+n_equipment = ("equipment_std", "nunique"), 
+n_target_muscles = ("target_muscle_std", "nunique"), 
+mean_rating = ("rating", "mean"), 
+median_rating = ("rating", "median"), 
+sd_rating = ("rating", "std") 
+) 
+.reset_index() 
+) 
+# Dimensiones del nuevo DataFrame 
+df_usuario.shape 
+# Impresión de las primeras filas 
+df_usuario.head() 
+# Calculando el conteo de objetivos por usuario 
+conteo_objetivos = pd.crosstab( 
+df["user_id"], 
+df["fitness_goal_std"] 
+) 
+# Añadiendo al nombre de cada variable el prefijo n_goal_ 
+conteo_objetivos = conteo_objetivos.add_prefix("n_goal_") 
+# Calculando el conteo de partes del cuerpo ejercitadas por usuario 
+conteo_bodyparts = pd.crosstab( 
+    df["user_id"], 
+    df["bodyPart_std"] 
+) 
+# Calculando el conteo de partes del cuerpo ejercitadas por usuario 
+conteo_bodyparts = pd.crosstab( 
+    df["user_id"], 
+    df["bodyPart_std"] 
+) 
+# Agregando variables que cuentan objetivos 
+df_usuario = df_usuario.merge( 
+    conteo_objetivos, 
+    on = "user_id", 
+    how = "left" 
+) 
+# Agregando variables que cuentan partes del cuerpo ejercitadas 
+df_usuario = df_usuario.merge( 
+    conteo_bodyparts, 
+    on="user_id", 
+    how="left" 
+) 
+# Proporción de cantidad de registros en general fitness 
+df_usuario["share_general_fitness"] = ( 
+    df_usuario["n_goal_general_fitness"] 
+    / df_usuario["n_records"] 
+) 
+# Proporción de cantidad de registros en endurance 
+df_usuario["share_endurance"] = ( 
+    df_usuario["n_goal_endurance"] 
+    / df_usuario["n_records"] 
+) 
+df_usuario["user_id"].nunique() 
+df_usuario.shape[0]
+df_usuario["user_id"].duplicated().sum() 
